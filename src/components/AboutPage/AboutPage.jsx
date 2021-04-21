28ae46133f0419fff807b67d2ee7b7420d470c8a
Tom Schrier <tomschr@stud.ntnu.no>
2021-04-21 11:46:24 +0200

    WIP

---
+++ src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,12 @@
+import './AboutPage.css';
+
+function AboutPage() {
+
+    return (
+        <div className="hero-image">
+            <p>This is the about page</p>
+        </div>
+    )
+}
+
+export default AboutPage;
