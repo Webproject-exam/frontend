69a8f5b1d950d1595d980209ec9002154ca5c429
Tom Schrier <tomschr@stud.ntnu.no>
2021-04-22 19:20:08 +0200

    About Page WIP

---
+++ src/components/AboutPage/AboutPage-old.jsx
@@ -0,0 +1,16 @@
+import './AboutPage.css';
+
+function AboutPage() {
+
+    return (
+        <>
+            <div className="hero-image"><div className="frosted-layer"></div></div>
+            
+                <div className="hope-page-contant">djhfsjdfhksdhfksjdfkjsdfhskdfjh</div>
+            
+        </>
+
+    )
+}
+
+export default AboutPage;
