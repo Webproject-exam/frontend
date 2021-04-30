d4ed3af998b93fd74306f4928e66a5ab83309106
Glenn Hansen <hansenglenneirik@gmail.com>
2021-04-30 14:55:59 +0200

    Split dashboard to serve both plants and users

---
+++ src/components/Dashboard/DashboardPlants.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class DashboardPlants extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <p>Hei din jøde</p>
+        );
+    }
+}
+
+export default DashboardPlants;
