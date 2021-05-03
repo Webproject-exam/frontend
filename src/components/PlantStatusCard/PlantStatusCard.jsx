0fbfd2227bd16dbcbc188e175b5fa4429207baeb
Tom <54577949+TomSchrier@users.noreply.github.com>
2021-05-03 19:23:31 +0200

    E

--- src/components/PlantStatusCard/PlantStatusCard.jsx
+++ src/components/PlantStatusCard/PlantStatusCard.jsx
@@ -13,67 +13,80 @@ function PlantStatusCard(props) {
                 <h2 className="h2">Status</h2>
                 <PlantStatusCircle next_watering={props.plant.watering.waterNext} />
             </div>
-            <ul>
-                <li>
-                    <span className="bold">Placement:</span> {props.plant.placement.building} – {props.plant.placement.floor} – {props.plant.placement.room}
-                </li>
-            </ul>
+            <section>
+                <h3>Placement</h3>
+                <ul>
+                    <li>
+                        <span className="bold">Building:</span> {props.plant.placement.building}
+                    </li>
+                    <li>
+                        <span className="bold">Floor:</span> {props.plant.placement.floor}
+                    </li>
+                    <li>
+                        <span className="bold">Room:</span> {props.plant.placement.room}
+                    </li>
+                </ul>
+            </section>
             <section>
                 <h3>Watering</h3>
-                <li>
-                    <span className="bold">Watering frequency:</span> every {props.plant.watering.waterFrequency} days
-                </li>
+                <ul>
+                    <li>
+                        <span className="bold">Watering frequency:</span> every {props.plant.watering.waterFrequency} days
+                    </li>
 
-                <li>
-                    <span className="bold">Next watering:</span> {myTimeConverter(props.plant.watering.waterNext)}
-                </li>
+                    <li>
+                        <span className="bold">Next watering:</span> {myTimeConverter(props.plant.watering.waterNext)}
+                    </li>
 
-                <li>
-                    <span className="bold">To be watered by:</span> {props.plant.watering.responsible}
-                </li>
+                    <li>
+                        <span className="bold">To be watered by:</span> {props.plant.watering.responsible}
+                    </li>
 
-                <li>
-                    <span className="bold">Last watered by:</span> {props.plant.watering.last_watered_by}
-                </li>
+                    <li>
+                        <span className="bold">Last watered by:</span> {props.plant.watering.last_watered_by}
+                    </li>
 
-                <li>
-                    <span className="bold">Watering was last postponed:</span> {props.plant.watering.last_postponed ? format(parseISO(props.plant.watering.last_postponed), 'PPP') : 'never'}
-                </li>
+                    <li>
+                        <span className="bold">Watering was last postponed:</span> {props.plant.watering.last_postponed ? format(parseISO(props.plant.watering.last_postponed), 'PPP') : 'never'}
+                    </li>
 
-                <li>
-                    <span className="bold">Reasoning for the last postponement:</span> "{props.plant.watering.postponed_reason}"
-                </li>
+                    <li>
+                        <span className="bold">Reasoning for the last postponement:</span> "{props.plant.watering.postponed_reason}"
+                    </li>
+                </ul>
             </section>
 
             <section>
                 <h3>fertilization</h3>
-                <li>
-                    <span className="bold">Fertilized frequency:</span> every {props.plant.fertilization.fertFrequency} days
-                </li>
+                <ul>
+                    <li>
+                        <span className="bold">Fertilized frequency:</span> every {props.plant.fertilization.fertFrequency} days
+                    </li>
 
-                <li>
-                    <span className="bold">Next fertilization: </span> {myTimeConverter(props.plant.fertilization.fertNext)}
-                </li>
+                    <li>
+                        <span className="bold">Next fertilization: </span> {myTimeConverter(props.plant.fertilization.fertNext)}
+                    </li>
 
-                <li>
-                    <span className="bold">Fertilization was last postponed:</span> {props.plant.fertilization.last_postponed ? format(parseISO(props.plant.fertilization.last_postponed), 'PPP') : 'never'}</li>
-
-                <li>
-                    <span className="bold">Reasoning for the last postponement:</span> "{props.plant.watering.postponed_reason}"
-                </li>
+                    <li>
+                        <span className="bold">Fertilization was last postponed:</span> {props.plant.fertilization.last_postponed ? format(parseISO(props.plant.fertilization.last_postponed), 'PPP') : 'never'}</li>
+                    <li>
+                        <span className="bold">Reasoning for the last postponement:</span> "{props.plant.watering.postponed_reason}"
+                    </li>
+                </ul>
             </section>
 
             <section>
                 <h3>Other</h3>
-                <li>
-                    <span className="bold">Recommended lighting: </span> {props.plant.lighting}
-                </li>
+                <ul>
+                    <li>
+                        <span className="bold">Recommended lighting: </span> {props.plant.lighting}
+                    </li>
 
-                <li>
-                    <span className="bold">Has been a part of Mustad since:</span> {format(parseISO(props.plant.createdAt), 'PPP')}
-                </li>
+                    <li>
+                        <span className="bold">Has been a part of Mustad since:</span> {format(parseISO(props.plant.createdAt), 'PPP')}
+                    </li>
+                </ul>
             </section>
-
         </div>
     )
 }
