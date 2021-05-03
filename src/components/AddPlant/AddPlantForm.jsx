46019e496a4a6d3e807aeb37de0cbcc66855fa88
Glenn Hansen <hansenglenneirik@gmail.com>
2021-05-03 14:24:26 +0200

    Fixed response object

--- src/components/AddPlant/AddPlantForm.jsx
+++ src/components/AddPlant/AddPlantForm.jsx
@@ -17,6 +17,7 @@ class AddPlantForm extends Component {
             information_description: '',
             information_placement: '',
             information_watering: '',
+            information_nutrition: '',
 
             plantname: '',
             watering_frequency: '',
@@ -34,22 +35,30 @@ class AddPlantForm extends Component {
     handleSubmit = async (event) => {
         event.preventDefault();
 
+        const waterFreq = parseInt(this.state.watering_frequency);
+        const fertFreq = parseInt(this.state.fertilizing_frequency);
+
         const plantObject = {
             name: this.state.plantname,
             information: {
                 description: this.state.information_description,
                 placement: this.state.information_placement,
-                watering: this.state.information_watering
+                watering: this.state.information_watering,
+                nutrition: this.state.information_nutrition
             },
             placement: {
                 building: this.state.placement_building,
                 floor: this.state.placement_floor,
                 room: this.state.placement_room
             },
-            waterFreq: this.state.watering_frequency,
-            fertFreq: this.state.fertilizing_frequency,
-            fertAmount: this.state.fertilizer_amount,
-            lightAmount: this.state.lighting_requirements
+            watering: {
+                waterFrequency: waterFreq
+            },
+            fertilization: {
+                fertFrequency: fertFreq,
+                fertAmount: this.state.fertilizer_amount,
+            },
+            lighting: this.state.lighting_requirements
         };
 
         await this.props.onSubmitHandler(plantObject);
