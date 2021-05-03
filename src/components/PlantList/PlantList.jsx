e0724b66cd5f7482859962e39581a4ee75b88870
Glenn Hansen <hansenglenneirik@gmail.com>
2021-05-03 19:43:30 +0200

    Minor bug fixes

--- src/components/PlantList/PlantList.jsx
+++ src/components/PlantList/PlantList.jsx
@@ -3,12 +3,12 @@ import PlantListItem from '../PlantListItem/PlantListItem';
 import Header from '../Header/Header'
 
 function PlantList(props) {
-    const { plants, selectPlant } = props;
+    const { plants } = props;
     return (
         <>
             <Header heading="Overview"/>
             <ul className="plant-list">
-                {plants.map((plants) => (<PlantListItem selectPlant={selectPlant} key={plants.name} plant={plants}/>))}
+                {plants.map((plants) => (<PlantListItem key={plants.name} plant={plants}/>))}
             </ul>
         </>
     );
