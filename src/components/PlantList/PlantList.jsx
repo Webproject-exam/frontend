3ee8902dd7581dcf3da477ad5c0bead72b5dfe90
Tom Schrier <tomschr@stud.ntnu.no>
2021-05-06 14:26:10 +0200

    WIP

--- src/components/PlantList/PlantList.jsx
+++ src/components/PlantList/PlantList.jsx
@@ -3,12 +3,12 @@ import PlantListItem from '../PlantListItem/PlantListItem';
 import Header from '../Header/Header'
 
 function PlantList(props) {
-    const { plants } = props;
+    const { plants, auth } = props;
     return (
         <>
             <Header heading="Overview"/>
             <ul className="plant-list">
-                {plants.map((plants) => (<PlantListItem key={plants._id} plant={plants}/>))}
+                {plants.map((plants) => (<PlantListItem key={plants._id} plant={plants} auth={auth} handleWateringClick/>))}
             </ul>
         </>
     );
