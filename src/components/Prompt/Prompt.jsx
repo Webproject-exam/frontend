04c0602fff99c8ecc8299e2113ffdfab55ab420d
Glenn Hansen <hansenglenneirik@gmail.com>
2021-05-07 19:48:02 +0200

    new popup integration

--- src/components/Prompt/Prompt.jsx
+++ src/components/Prompt/Prompt.jsx
@@ -6,35 +6,44 @@ function Prompt(props) {
 
     let buttonVariant;
     let buttonLabel;
+    let action;
 
-    if (type === 'delete'){
+    if (type === 'delete') {
+        action = 'delete';
         buttonVariant = 'danger';
-        buttonLabel = 'confirm deletion';
+        buttonLabel = 'delete';
     } else {
+        action = 'water';
         buttonVariant = 'secondary';
         buttonLabel = 'confirm';
     }
 
-return (
-    <div className="container">
-        <>
-            {user &&
-                <p>Are you sure you want to delete the user <span className="bold">{user.name} {user.surname}</span>?</p>
-            }
+    return (
+        <div className="container">
+            <>
+                {user &&
+                <>
+                    <h1>Delete a user</h1>
+                    <p>Are you sure you want to delete the user <span className="bold">{user.name} {user.surname}</span>?</p>
+                </>
+                }
 
-            {plant &&
-                <p>Are you sure you want to water the plant <span className="bold">{plant.name}</span>?</p>
-            }
+                {plant &&
+                <>
+                    <h1>{action === 'delete' ? 'Delete' : 'Water'} a plant</h1>
+                    <p>Are you sure you want to {action} the plant <span className="bold">{plant.name}</span>?</p>
+                </>
+                }
 
-            <p className="low-emphasis-text">This action can not be undone!</p>
+                <p className="low-emphasis-text">This action can not be undone!</p>
 
-            <div className="buttons-side-by-side">
-                <Button onClick={onCancelClick} label="Cancel" size="half" variant="secondary-outlined" />
-                <Button onClick={onConfirmClick} label={buttonLabel} size="half" variant={buttonVariant} />
-            </div>
-        </>
-    </div>
-);
+                <div className="buttons-side-by-side">
+                    <Button onClick={onCancelClick} label="Cancel" size="half" variant="secondary-outlined" />
+                    <Button onClick={onConfirmClick} label={buttonLabel} size="half" variant={buttonVariant} />
+                </div>
+            </>
+        </div>
+    );
 }
 
 export default Prompt;
