fd73432c8e6eb1bc1089fb6be27acedf20b7b159
Glenn Hansen <hansenglenneirik@gmail.com>
2021-05-11 16:33:02 +0200

    IMage now works

--- src/components/PlantListItem/PlantListItem.jsx
+++ src/components/PlantListItem/PlantListItem.jsx
@@ -5,31 +5,38 @@ import PlantStatusCircle from '../PlantStatusCircle/PlantStatusCircle';
 import { Link } from "react-router-dom";
 import { myTimeConverter } from '../../helpers/timeConverter'
 
-function PlantListItem({plant, auth, handleWateringClick}) {
+function PlantListItem({ plant, auth, handleWateringClick }) {
     const location = `${plant.placement.building} – #${plant.placement.floor} – ${plant.placement.room}`;
+    let plantImage;
+    if (!plant.image) {
+        plantImage = Image;
+    } else {
+        plantImage = process.env.REACT_APP_IMAGE + plant.image;
+    }
+
     return (
-    <li>
-        <div className="plant-container">
-        <Link to={'/plants/' + plant._id}>
-                <div className="clickable">
-                    <div className="plant-header">
-                        <PlantStatusCircle waterNext={plant.watering.waterNext}/>
-                        <h2 className="h2">{plant.name}</h2>
-                        <h3 className="h3">{location}</h3>
+        <li>
+            <div className="plant-container">
+                <Link to={'/plants/' + plant._id}>
+                    <div className="clickable">
+                        <div className="plant-header">
+                            <PlantStatusCircle waterNext={plant.watering.waterNext} />
+                            <h2 className="h2">{plant.name}</h2>
+                            <h3 className="h3">{location}</h3>
+                        </div>
+
+                        <img className="plant-image" src={plantImage} alt={plant.name} />
                     </div>
-                    
-                    <img className="hero-image" src={Image} alt="plant" />
-                </div>
                 </Link>
-                
+
                 <p>Next watering: <span className="bold">{myTimeConverter(plant.watering.waterNext)}</span>.</p>
                 <p>Lighting: <span className="bold">{plant.lighting}</span>.</p>
                 <p>Fertilizer: <span className="bold">{plant.fertilization.fertAmount}</span></p>
-            
-            {auth && (<Button className="on-top" variant="text-only" label="water this plant" size="auto" onClick={() => handleWateringClick(plant)}/>)}
-        </div>
-        
-    </li>
+
+                {auth && (<Button className="on-top" variant="text-only" label="water this plant" size="auto" onClick={() => handleWateringClick(plant)} />)}
+            </div>
+
+        </li>
     )
 }
 
