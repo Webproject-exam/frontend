4078d362cbcce3d25be18e1cdcb70ff4d34307cf
Glenn Hansen <hansenglenneirik@gmail.com>
2021-05-21 11:44:57 +0200

    Added a masterful rickroll

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+console.log("Snooping around are we? Check out our video explaining the project here: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
